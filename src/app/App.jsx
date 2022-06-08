771665451e7783079e6572ab7e394d31f5427b62
milad <miladbn@noay.ir>
2022-06-08 08:22:59 +0430

    first commit

---
+++ src/app/App.jsx
@@ -0,0 +1,12 @@
+import DatePicker from "./datePicker";
+import calsses from "./app.module.css";
+
+function App() {
+  return (
+    <div className={calsses.app}>
+      <DatePicker />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/datePicker/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from "./datePicker.module.css";
+
+const DatePicker = () => {
+  return (
+    <div className={classes.datePickerMain}>
+      <div className={classes.firstRow}>
+        <div>1</div>
+        <div>2</div>
+      </div>
+    </div>
+  );
+};
+
+export default DatePicker;
