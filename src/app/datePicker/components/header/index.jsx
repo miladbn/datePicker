eafaab2cd1e95bcdd3d97059920b834cfbc9397a
milad <miladbn@noay.ir>
2022-06-08 09:46:51 +0430

    .

--- src/app/datePicker/components/header/index.jsx
+++ src/app/datePicker/components/header/index.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import YearPicker from "./yearPicker";
+import MonthPicker from "./monthPicker";
+
+import classes from "./header.module.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={classes.headerRow}>
+      <YearPicker />
+      <MonthPicker />
+    </div>
+  );
 };
 
 export default Header;
